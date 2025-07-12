3a09e83a6b0b1ea0d0ed13c2be92a7e5e2941e79
Anant <anantji2332@gmail.com>
2025-07-12 14:49:17 +0530

    Project Intialized and Setup Completed with ShadCn UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from App!</h1>
+      <Button>Click ME</Button>
+    </div>
+  );
+};
+
+export default App;
